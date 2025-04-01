d51fd3612a639e9f2c631d0a53fb1cdf9b281308
busea77 <buseatik96@gmail.com>
2025-04-01 15:57:52 +0300

    ilk güncelleme

---
+++ pages/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.sidebar-toggle').addEventListener('click', function() {
+        document.querySelector('#sidebar').classList.toggle('active');
+    });
+});
